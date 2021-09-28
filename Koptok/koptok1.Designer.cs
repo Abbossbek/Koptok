8dec7f992c5b046218f1bb727523fe95bd71f019
Abbosbek Niyozqulov <abbos6882@gmail.com>
2021-09-28 12:38:48 +0500

    Добавьте файлы проекта.

---
+++ Koptok/koptok1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace Koptok
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.White;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(508, 414);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDoubleClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.button1.Location = new System.Drawing.Point(237, 387);
+            this.button1.Margin = new System.Windows.Forms.Padding(0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "START";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox2.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(508, 414);
+            this.pictureBox2.TabIndex = 4;
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBox1.Location = new System.Drawing.Point(296, 183);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(97, 179);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(178, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Aylananing radiusi:";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.textBox2.Location = new System.Drawing.Point(296, 225);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 7;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(84, 221);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(191, 25);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Harakatlanish tezligi:";
+            // 
+            // Form1
+            // 
+            this.AcceptButton = this.button1;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.ClientSize = new System.Drawing.Size(532, 433);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Koptok";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Koptok/koptok1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Koptok
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int R, ok=0, st=0;
+        private Random rn = new Random(); Timer t = new Timer();
+        private int r, g, b, x=10, y=1, i=0, j=0;
+        private Graphics gr; Pen p, pb; Brush br; Graphics[] tr;
+        private bool xb=true, yb=true;
+        private int[] ab, ba;
+        
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            BackColor = Color.FromArgb(r, g, b);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                r = rn.Next(0, 255); g = rn.Next(0, 255); b = rn.Next(0, 255);
+                BackColor = Color.FromArgb(r, g, b);
+                if (textBox1.Text != "" && textBox2.Text!="")
+                {
+                    ok++;
+                    if (ok == 1)
+                    {
+                        button1.Text = "Clear";
+                        button1.SendToBack();
+                        pictureBox2.SendToBack();
+                        label1.SendToBack();
+                        textBox1.SendToBack();
+                        label2.SendToBack();
+                        textBox2.SendToBack();
+                        R = Convert.ToInt32(textBox1.Text);
+                        for (int tez = 0; tez < int.Parse(textBox2.Text); tez++)
+                        {
+                            t.Interval = 1;
+                            t.Tick += new EventHandler(timer1_Tick);
+                        }
+                    }
+                    tr = new Graphics[10000];
+                    p = new Pen(Color.FromArgb(r, g, b),3);
+                    br = new SolidBrush(Color.FromArgb(255-r, 255-g, 255-b));
+                    r = rn.Next(0, 255); g = rn.Next(0, 255); b = rn.Next(0, 255);
+                    BackColor = Color.FromArgb(r, g, b);
+                }
+            }
+            catch
+            {
+                textBox1.Focus();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (i < 1)
+            {
+                gr = pictureBox1.CreateGraphics();
+                pb = new Pen(Color.White, 3);
+                br = new SolidBrush(Color.White);
+                gr.DrawEllipse(p, x, y, R, R);
+                if (x == 0)
+                {
+                    xb = true;
+                    r = rn.Next(0, 255); g = rn.Next(0, 255); b = rn.Next(0, 255);
+                    p = new Pen(Color.FromArgb(r, g, b),3);
+                }
+                if (x == pictureBox1.Width - R)
+                {
+                    xb = false;
+                    r = rn.Next(0, 255); g = rn.Next(0, 255); b = rn.Next(0, 255);
+                    p = new Pen(Color.FromArgb(r, g, b),3);
+                }
+                if (y == 0)
+                {
+                    yb = true;
+                    r = rn.Next(0, 255); g = rn.Next(0, 255); b = rn.Next(0, 255);
+                    p = new Pen(Color.FromArgb(r, g, b),3);
+                }
+                if (y == pictureBox1.Height - R)
+                {
+                    yb = false;
+                    r = rn.Next(0, 255); g = rn.Next(0, 255); b = rn.Next(0, 255);
+                    p = new Pen(Color.FromArgb(r, g, b),3);
+                }
+                gr.DrawEllipse(pb, x, y, R, R);
+                x = xb == true ? x + 1 : x - 1;
+                y = yb == true ? y + 1 : y - 1;
+            }
+        }
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            r = rn.Next(0, 255); g = rn.Next(0, 255); b = rn.Next(0, 255);
+            br = new SolidBrush(Color.FromArgb(255 - r, 255 - g, 255 - b));
+            gr = pictureBox1.CreateGraphics();
+            ab =new int[10000]; ba = new int[10000];
+            for(int i=0; i<10000; i++)
+            {
+                ab[i]=rn.Next(10,200);
+                ba[i]=rn.Next(10,200);
+            }
+            tr[j] = pictureBox1.CreateGraphics();
+            r = rn.Next(0, 255); g = rn.Next(0, 255); b = rn.Next(0, 255);
+            tr[j].FillRectangle(br, e.X, e.Y , ab[j], ba[j]);
+            j++;
+            st++;
+        }
+        private void pictureBox1_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+            if (st == 1)
+            {
+                gr.DrawEllipse(p, e.X - R / 2, e.Y - R / 2, R, R);
+            }
+            else
+            {
+                if (e.X > R / 2 && e.Y > R / 2 && e.X < pictureBox1.Width - R / 2 && e.Y < pictureBox1.Height - R / 2)
+                {
+                    x = e.X - R / 2;
+                    y = e.Y - R / 2;
+                    p = new Pen(Color.FromArgb(r, g, b), 3);
+                    br = new SolidBrush(Color.FromArgb(255 - r, 255 - g, 255 - b));
+                    t.Start();
+                }
+            }
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= '0' && e.KeyChar <= '9')
+            {
+                return;
+            }
+            if (char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter)
+                {
+                    if (sender.Equals(textBox1))
+                        textBox2.Focus();
+                    else
+                        button1.Focus();
+                }
+                return;
+            }
+            e.Handled = true;
+        }
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar >= '0' && e.KeyChar <= '9')
+            {
+                return;
+            }
+            if (char.IsControl(e.KeyChar))
+            {
+                if (e.KeyChar == (char)Keys.Enter)
+                {
+                        button1.Focus();
+                }
+                return;
+            }
+            e.Handled = true;
+        }
+    }
+}
